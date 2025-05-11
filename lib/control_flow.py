cbc2c9c80c4559eac922345c5f6b0c9d03e43e1a
dancan <dancan.morara@student.moringaschool.com>
2025-05-11 17:01:16 +0300

    flow

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,53 @@
 
 def admin_login(username, password):
     # your code here
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
     pass
 
 def hows_the_weather(temperature):
     # your code here
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature > 40 and temperature < 60:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
     pass
 
 def fizzbuzz(num):
     # your code here
+    try:
+        if num % 3 == 0 and num % 5 == 0:
+            return "FizzBuzz"
+        elif num % 3 == 0:
+            return "Fizz"
+        elif num % 5 == 0:
+            return "Buzz"
+        else: 
+            return num
+    except ZeroDivisionError:
+        return "Error: Division by zero is not allowed."
     pass
 
 def calculator(operation, num1, num2):
     # your code here
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        try:
+            return num1 / num2
+        except ZeroDivisionError:
+            return "Error: Division by zero is not allowed."
+    else:
+        print("Invalid operation!")
+        return None    
     pass
